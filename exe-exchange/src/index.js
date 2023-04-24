58beac28b1c58738436bff86e2a704e9c91b9b70
Adarsh Goel <f20200635@pilani.bits-pilani.ac.in>
2023-04-24 17:04:44 +0530

    tuKarBhai

--- exe-exchange/src/index.js
+++ exe-exchange/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
 import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Router';
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const chains = [polygon, polygonMumbai];
+const projectId = "40435ba07c29dd9596260e4cb18ff938";
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}><App /></RouterProvider>
+    {" "}
+    <WagmiConfig client={wagmiClient}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>{" "}
+    </WagmiConfig>
   </React.StrictMode>
 );
 
