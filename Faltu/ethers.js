61784122877c856ff1717ed8061462d3f9e5a0d5
Scream-13 <109026449+Scream-13@users.noreply.github.com>
2023-04-24 14:22:42 +0530

    ehters.js_pushed

---
+++ Faltu/ethers.js
@@ -0,0 +1,17 @@
+const { ethers, network } = require("hardhat");
+
+
+const usdcAddress = 2
+const RouterAddress = 1
+const GridAddress = 2
+
+const RouterABI = 
+
+const GridABI = 
+
+const getProviderOrSigner = (bool takeSigner)=>{
+    
+
+}
+
+const routerObject = ethers.getContractAt(RouterABI, RouterAddress )
